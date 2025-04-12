cc8add2d5ad6c38f23cad98804ab1fa1c172fe15
mattphillips-one <mattphillips142@gmail.com>
2025-04-12 18:08:30 -0400

    Removed unnecessary imports

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
-import { Stack, useLocalSearchParams } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const { modalHeader } = useLocalSearchParams<{modalHeader: string}>();
 
   return (
     <Stack>
